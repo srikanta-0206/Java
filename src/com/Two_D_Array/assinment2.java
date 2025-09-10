9b919027c5b6f51e78949d8ad58e6ddac3cc08bf
srikanta-0206 <srikantasahu280@gmail.com>
2025-09-10 19:18:20 +0530

    first program

---
+++ src/com/Two_D_Array/assinment2.java
@@ -0,0 +1,11 @@
+package com.Two_D_Array;
+
+public class assinment2 {
+    public static void main(String[] args) {
+        int n =5;
+        int[][] a= new int[n][n];
+        int col=-1;
+        int row=0;
+
+    }
+}
